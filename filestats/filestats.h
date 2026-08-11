ca06bac02ef4d113b2c674fbe1aaf6f4cf7fbc4f
Ross K <256326267+Rossco1406@users.noreply.github.com>
2026-08-11 17:21:50 +0100

    Main.c Complete

---
+++ filestats/filestats.h
@@ -0,0 +1,9 @@
+#ifndef FILESTAT_H
+#define FILESTAT_H
+
+#include <stdio.h>
+
+void filestats(FILE *fin, FILE *fout);
+void printStats(FILE *fin, FILE *fout);
+
+#endif
---
+++ filestats/main.c
@@ -0,0 +1,48 @@
+#include "filestats.h"
+
+int main(int argc, char *argv[])
+{   
+    FILE *input;
+    FILE *output;
+    char *prog = argv[0];
+    
+    if (argc == 1)
+        filestats(stdin,stdout);
+    else if (argc == 2){
+        input = fopen(argv[1], "r");
+        if (input == NULL) {
+            fprintf(stderr, "%s:  %s doesnt exist\n", prog, argv[1]);
+            return 1;
+        }
+        filestats(input,stdout);
+        fclose(input);
+
+    }
+    else if (argc == 3){
+        input = fopen(argv[1], "r");
+
+        if (input == NULL) {
+            fprintf(stderr, "%s: cannot open %s for reading\n", prog, argv[1]);
+            return 1;
+        }
+
+        output = fopen(argv[2], "w");
+
+        if (output == NULL){
+            fprintf(stderr, "%s: cannot open %s for writing\n", prog, argv[2]);
+            fclose(input);
+            return 1;
+        }
+        
+        filestats(input, output);
+
+        fclose(input);
+        fclose(output);
+    }
+    else {
+        printf("Usage: %s [input] [output]\n", argv[0]);
+        return 1;
+    }
+
+    return 0;
+}
