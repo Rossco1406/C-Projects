51cff6e1737faa4e8ac9ec2cc0d69e4e9f57db18
Ross K <256326267+Rossco1406@users.noreply.github.com>
2026-09-19 12:33:11 +0100

    Concurrency added

--- mini_shell_env/src/server.c
+++ mini_shell_env/src/server.c
@@ -53,120 +53,136 @@ int main()
 
     printf("Server listening on port 8080\n");
 
-    int client_fd;
-
-    client_fd = accept(server_fd, NULL, NULL);
-
-    if (client_fd < 0)
+    while (1)
     {
-        perror("accept");
-        close(server_fd);
-        return 1;
+        int client_fd;
+
+        client_fd = accept(server_fd, NULL, NULL);
+
+        if (client_fd < 0)
+        {
+            perror("accept");
+            continue;
+        }
+
+        printf("Client connected\n");
+
+        pid_t pid = fork();
+
+        if (pid < 0)
+        {
+            perror("fork");
+            close(client_fd);
+            continue;
+        }
+
+        if (pid == 0)
+        {
+            close(server_fd);
+
+            while (1){
+
+                char buffer[1024];
+
+                ssize_t bytes_received = recv(client_fd, buffer, sizeof(buffer) - 1, 0);
+
+                if (bytes_received < 0)
+                {
+                    perror("recv");
+                    break;
+                }
+
+                if (bytes_received == 0)
+                {
+                    printf("Client disconnected\n");
+                    break;
+                }
+
+                buffer[bytes_received] = '\0';
+
+                if (strcmp(buffer, "exit") == 0)
+                {
+                    printf("Client requested disconnect\n");
+                    break;
+                }
+
+                int output_pipe[2];
+
+                if (pipe(output_pipe) < 0)
+                {
+                    perror("pipe");
+                    break;
+                }
+
+                int saved_stdout = dup(STDOUT_FILENO);
+
+                if (saved_stdout < 0)
+                {
+                    perror("dup");
+                    close(output_pipe[0]);
+                    close(output_pipe[1]);
+                    break;
+                }
+
+                if (dup2(output_pipe[1], STDOUT_FILENO) < 0)
+                {
+                    perror("dup2");
+                    close(output_pipe[0]);
+                    close(output_pipe[1]);
+                    close(saved_stdout);
+                    break;
+                }
+
+                close(output_pipe[1]);  
+
+                execute_command_string(buffer);
+
+                if (dup2(saved_stdout, STDOUT_FILENO) < 0)
+                {
+                    perror("dup2");
+                }
+
+                close(saved_stdout);
+
+                char output[4096];
+
+                ssize_t bytes_read = read(output_pipe[0],
+                                    output,
+                                    sizeof(output) - 1);
+
+                if (bytes_read < 0)
+                {
+                    perror("read");
+                    close(output_pipe[0]);
+                    break;
+                }
+
+                output[bytes_read] = '\0';
+
+                close(output_pipe[0]);
+
+                printf("Command output:\n%s", output);
+
+                if (send(client_fd, output, bytes_read, 0) < 0)
+                {
+                    perror("send");
+                    break;
+                }
+
+                printf("Response sent\n");
+            }
+
+            close(client_fd);
+            exit(0);
+        }
+        else
+        {
+            close(client_fd);
+        }
     }
 
-    printf("Client connected\n");
-
-    while (1){
-
-        char buffer[1024];
-
-        ssize_t bytes_received = recv(client_fd, buffer, sizeof(buffer) - 1, 0);
-
-        if (bytes_received < 0)
-        {
-            perror("recv");
-            close(client_fd);
-            close(server_fd);
-            return 1;
-        }
-
-        buffer[bytes_received] = '\0';
-
-        if (strcmp(buffer, "exit") == 0)
-        {
-            printf("Client requested disconnect\n");
-            break;
-        }
-
-        int output_pipe[2];
-
-        if (pipe(output_pipe) < 0)
-        {
-            perror("pipe");
-            close(client_fd);
-            close(server_fd);
-            return 1;
-        }
-
-        int saved_stdout = dup(STDOUT_FILENO);
-
-        if (saved_stdout < 0)
-        {
-            perror("dup");
-            close(output_pipe[0]);
-            close(output_pipe[1]);
-            close(client_fd);
-            close(server_fd);
-            return 1;
-        }
-
-        if (dup2(output_pipe[1], STDOUT_FILENO) < 0)
-        {
-            perror("dup2");
-            close(output_pipe[0]);
-            close(output_pipe[1]);
-            close(saved_stdout);
-            close(client_fd);
-            close(server_fd);
-            return 1;
-        }
-
-        close(output_pipe[1]);  
-
-        execute_command_string(buffer);
-
-        if (dup2(saved_stdout, STDOUT_FILENO) < 0)
-        {
-            perror("dup2");
-        }
-
-        close(saved_stdout);
-
-        char output[4096];
-
-        ssize_t bytes_read = read(output_pipe[0],
-                            output,
-                            sizeof(output) - 1);
-
-        if (bytes_read < 0)
-        {
-            perror("read");
-            close(output_pipe[0]);
-            close(client_fd);
-            close(server_fd);
-            return 1;
-        }
-
-        output[bytes_read] = '\0';
-
-        close(output_pipe[0]);
-
-        printf("Command output:\n%s", output);
-
-        if (send(client_fd, output, bytes_read, 0) < 0)
-        {
-            perror("send");
-            close(client_fd);
-            close(server_fd);
-            return 1;
-        }
-
-        printf("Response sent\n");
-    }
-
-    close(client_fd);
     close(server_fd);
-
     return 0;
+
 }
 
